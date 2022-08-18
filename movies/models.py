9517bdbe4e4486d0a164fd6742c31c718424a755
Steven Zaldivar <herrerachristian1897@gmail.com>
2022-08-18 15:58:44 -0600

    Update models
    Added RentalManager and migrations updated

--- movies/models.py
+++ movies/models.py
@@ -1,6 +1,5 @@
 from django.contrib.auth import get_user_model
-from django.db import models
-from django.dispatch import receiver
+from django.db import models, transaction
 
 # Create your models here.
 
@@ -13,11 +12,41 @@ class Movie(models.Model):
     name = models.CharField(max_length=1000, null=False)
     synopsis = models.CharField(max_length=2500, null=False)
     copies = models.IntegerField(null=False)
+    rented_copies = models.IntegerField(null=True, default=0)
     created_at = models.DateTimeField(auto_now_add=True)
     last_modification = models.DateTimeField(auto_now=True)
 
     def __str__(self) -> str:
         return self.name
+
+
+class RentalsManager(models.Manager):
+    
+    @transaction.atomic()
+    def remove_lend(self, id_: int):
+        try:
+            obj = self.get(rental_id=id_)
+            copies = obj.copies
+            movie_id = obj.movie_id
+            obj.delete()
+        except Exception as e:
+            print(e)
+            raise e
+        else:
+            try:
+                movie: Movie = Movie.objects.get(pk=movie_id.movie_id)
+                movie.rented_copies -= copies
+                movie.save()
+            except Exception as e:
+                raise e
+            
+    
+    def get_rented_copies(self, movie_id) -> int:
+        try:
+            return self.filter(movie_id_id=movie_id).aggregate(rented_copies=models.Sum('copies'))
+        except Exception as e:
+            print(e)
+            raise
 
 class Rentals(models.Model):
     
@@ -34,3 +63,4 @@ class Rentals(models.Model):
     max_date = models.DateField(null=False)
     
     
+    objects = RentalsManager()
