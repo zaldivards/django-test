2793d6fea9ddf25c971afc94194a178a4c392db4
Steven Zaldivar <herrerachristian1897@gmail.com>
2022-08-18 15:59:51 -0600

    Update settings
    Custom css classes set for box messages

--- challenge_vo/settings.py
+++ challenge_vo/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 
 from decouple import config
+from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -69,6 +70,11 @@ TEMPLATES = [
         },
     },
 ]
+
+MESSAGE_TAGS = {
+        messages.SUCCESS: 'alert-success',
+        messages.ERROR: 'alert-danger',
+ }
 
 WSGI_APPLICATION = 'challenge_vo.wsgi.application'
 
