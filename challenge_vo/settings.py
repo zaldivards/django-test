9e514cd3b26f5744600fad60d4ef2007f24bddcb
Steven Zaldivar <herrerachristian1897@gmail.com>
2022-08-18 22:47:47 -0600

    Add app <api> to implement the rest api

--- challenge_vo/settings.py
+++ challenge_vo/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework'
     'movies'
 ]
 
@@ -130,3 +131,10 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ]
+}
