44d2f033083ad4905aa1ee9a871fe54162a4f0dd
Steven Zaldivar <herrerachristian1897@gmail.com>
2022-08-18 23:55:13 -0600

    Add openapi documentation

--- challenge_vo/urls.py
+++ challenge_vo/urls.py
@@ -15,8 +15,29 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+...
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Movie rentals API",
+      default_version='v1',
+      description="Open api test",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="herrerachristian@gmail.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
+
+
 
 urlpatterns = [
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('movies/', include(('movies.urls', 'movies'), namespace="movies")),
     path('api/v1/', include('api.urls')),
     path('admin/', admin.site.urls, name='admin')
